9de6d8c7517ca5f125dded947d3dbdd1a0a20f59
CBZakaria <cbzakaria95@gmail.com>
2018-10-11 18:58:05 +0200

    feat(): Un module que pour  AngularMateriel

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * Dans ce module on va importer tous ce qui est en relation avec Angular Material
+ */
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
