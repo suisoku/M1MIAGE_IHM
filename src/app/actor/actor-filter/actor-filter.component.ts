daf9e8cab617803b8f79556f6e29d35f4e440e7c
CBZakaria <cbzakaria95@gmail.com>
2018-10-11 19:35:34 +0200

    feat(): Composant pour les acteurs et le filtre acteur

---
+++ src/app/actor/actor-filter/actor-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actor-filter',
+  templateUrl: './actor-filter.component.html',
+  styleUrls: ['./actor-filter.component.css']
+})
+export class ActorFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
