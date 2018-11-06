8cb66b1ee058c7c330830c0c65456904a6042f5c
SidsonGGMU <sidimaidara@gmail.com>
2018-11-06 01:01:34 +0100

    feat() : added actor details component
    Displays every actor-related information.

---
+++ src/app/actor-details/actor-details.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TmdbService } from '../tmdb.service';
+import { ActivatedRoute } from '@angular/router';
+import { PersonResponse, PersonDetails } from '../tmdb-data/Person';
+
+@Component({
+  selector: 'app-actor-details',
+  templateUrl: './actor-details.component.html',
+  styleUrls: ['./actor-details.component.css']
+})
+export class ActorDetailsComponent implements OnInit, OnDestroy {
+
+  private actorDetails: PersonDetails; // Film en cours
+  private id: any;
+  private routeSubscription: any;
+
+  constructor(private tmdb: TmdbService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.routeSubscription = this.route.params.subscribe((param: any) => {
+      this.id = param['id'];
+      this.tmdb.init('af82599daa1c8b9cef254d429ec0d436') // Clef de TMDB
+      this.loadCurrentActor();
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
+  }
+
+  loadCurrentActor() {
+    this.tmdb.getPersonDetails(this.id)
+      .then((m: PersonDetails) => {
+        console.log('Actor response:', this.actorDetails = m);
+        this.actorDetails.known_for.cast = m.known_for.cast.slice(0, 5);
+      })
+      .catch(err => console.error('Error getting movie:', err))
+  }
+  get actor(): any {
+    return this.actorDetails;
+  }
+
+  getPath(path: string): string {
+    return this.tmdb.getPath(path);
+  }
+
+
+}
